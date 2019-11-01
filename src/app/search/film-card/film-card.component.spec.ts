9c2ed3bc4f6164d69c187c9e23639303a30dba11
Gurpreet Bhopal <gbhopal91@gmail.com>
2019-11-01 09:50:40 +0000

    test for card

--- src/app/search/film-card/film-card.component.spec.ts
+++ src/app/search/film-card/film-card.component.spec.ts
@@ -1,6 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FilmCardComponent } from './film-card.component';
+import { MaterialModule } from '../../material.module';
+import { Film } from '../search.models';
+
+const mockFilm: Film = {
+  original_title: 'Test',
+  title: 'Test',
+  overview: 'Testing',
+  vote_average: 12,
+  poster_path: 'Test',
+};
 
 describe('FilmCardComponent', () => {
   let component: FilmCardComponent;
@@ -8,6 +18,7 @@ describe('FilmCardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MaterialModule],
       declarations: [FilmCardComponent],
     }).compileComponents();
   }));
@@ -15,10 +26,16 @@ describe('FilmCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FilmCardComponent);
     component = fixture.componentInstance;
+    component.film = mockFilm;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set mat card header with the title from film', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-card-title').textContent).toContain(mockFilm.title);
   });
 });
