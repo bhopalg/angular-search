fa98687873dd75751b16551b847835045d630ef0
Gurpreet Bhopal <gbhopal91@gmail.com>
2019-11-01 10:39:34 +0000

    fix lint

--- src/app/auto-unsubscribe/auto-unsubscribe.ts
+++ src/app/auto-unsubscribe/auto-unsubscribe.ts
@@ -1,9 +1,9 @@
 export function AutoUnsubscribe(blackList: string[] = []): Function {
-  return function(constructor) {
+  return (constructor: any): any => {
     const original = constructor.prototype.ngOnDestroy;
 
-    constructor.prototype.ngOnDestroy = function() {
-      for (let prop in this) {
+    constructor.prototype.ngOnDestroy = (): any => {
+      for (const prop of this) {
         const property = this[prop];
         if (!blackList.includes(prop)) {
           if (property && typeof property.unsubscribe === 'function') {
@@ -11,7 +11,9 @@ export function AutoUnsubscribe(blackList: string[] = []): Function {
           }
         }
       }
+      /* tslint:disable */
       original && typeof original === 'function' && original.apply(this, arguments);
+      /* tslint:enable */
     };
   };
 }
